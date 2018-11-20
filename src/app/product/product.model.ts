27b4e61252e3ea6cd69302af78e2fb930f605110
Michael Zhang <Michael@192.168.0.104>
2018-11-20 10:10:46 +0800

    implement product component basic UI logic

---
+++ src/app/product/product.model.ts
@@ -0,0 +1,13 @@
+export class Product {
+    constructor(
+      public id: number,
+      public title: string,
+      public price: number,
+      public rating: number,
+      public categories: Array<string>,
+      public desc: string,
+      public imgUrl: string,
+    ) {
+  
+    }
+  }
