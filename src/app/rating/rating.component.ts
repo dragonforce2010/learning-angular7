fa72b8774c14524a943c5b039edcf0336a112fcb
Michael Zhang <Michael@Gangs-MacBook-Air.local>
2018-11-20 17:20:16 +0800

    - fix bootstrap does not support icon issue - finished rating component

--- src/app/rating/rating.component.ts
+++ src/app/rating/rating.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-rating',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rating.component.css']
 })
 export class RatingComponent implements OnInit {
+  private starEmptyness: boolean[]
+  @Input()
+  private rating: number = 0
 
   constructor() { }
 
   ngOnInit() {
+    this.starEmptyness = []
+    for (let i = 0; i <=5; i++) {
+      this.starEmptyness.push(i > this.rating)
+    }
   }
 
 }
